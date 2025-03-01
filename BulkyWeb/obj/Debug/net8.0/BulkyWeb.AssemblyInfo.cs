4471a3cd28ff4da4df061d648e18807edfd71527
Calmen <calmenchia@gmail.com>
2025-03-01 12:48:53 +0800

    Configure Identity Scaffolded

--- BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
+++ BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf78a9959c8f6b6129bb6e780899decd7565244e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc31182d83462d180452ec4ae3a09dd064d8c389")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
