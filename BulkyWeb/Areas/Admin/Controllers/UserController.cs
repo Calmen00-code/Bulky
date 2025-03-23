b03651f5f18a40c07389e92300380378592fab81
Calmen <calmenchia@gmail.com>
2025-03-23 16:16:11 +0800

    Added manage user for content management under admin

---
+++ BulkyWeb/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,66 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using Bulky.Models.ViewModels;
+using Bulky.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.ROLE_ADMIN)]
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UserController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        // this allow API to be called by external sources
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<ApplicationUser> users = _db.ApplicationUsers.Include(u => u.Company).ToList();
+
+            var userRoles = _db.UserRoles.ToList();
+            var roles = _db.Roles.ToList();
+
+            foreach (var user in users)
+            {
+                var roleId = userRoles.FirstOrDefault(u => u.UserId == user.Id).RoleId;
+                user.Role = roles.FirstOrDefault(u => u.Id == roleId).Name;
+
+                if (user.Company == null)
+                {
+                    user.Company = new() { Name = "" };
+                }
+            }
+
+            return Json(new {data = users});
+        }
+
+        // remove this HttpDelete tag when u are not using SweetAlert
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+
+            return Json(new {success = true, message = "Deleted Successful"});
+        }
+
+        #endregion
+    }
+}
